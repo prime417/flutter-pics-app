4b3519c7a479f65f4e35c6ec3b9e0ba4e3a3df6f
prime417 <pankajds417@gmail.com>
2018-12-21 18:52:55 +0530

    define main function

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,10 @@
 // from flutter to get content on the screen
 import 'package:flutter/material.dart';
 
-
 // Define a 'main' function to run when our app starts
-
-
+void main() {
 // Create a new text widget to show some text
-// on the screen 
+// on the screen
 
 //Take that widget and get it on the screen
+}
