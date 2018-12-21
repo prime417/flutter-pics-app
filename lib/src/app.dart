ee3164c1189f66dbc4a3b0a58f89988adb014bfb
prime417 <pankajds417@gmail.com>
2018-12-21 23:10:09 +0530

    app.dart file created and comments added

---
+++ lib/src/app.dart
@@ -0,0 +1,7 @@
+//Import flutter helper library
+
+// Create a class that will be our custom widget
+// This class must extend the 'StatlessWidget' base class
+
+// Must define a 'build' method that returns
+// the widgets that *this* widget will show
