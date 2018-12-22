ef192c080f07550cab2a096a1a79fc2c120b4fe0
prime417 <pankajds417@gmail.com>
2018-12-22 18:00:16 +0530

    created custom widget app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,24 @@
 //Import flutter helper library
+import 'package:flutter/material.dart';
 
 // Create a class that will be our custom widget
 // This class must extend the 'StatlessWidget' base class
-
+class App extends StatelessWidget {
 // Must define a 'build' method that returns
 // the widgets that *this* widget will show
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Lets see some images!'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            print('Hi there!');
+          },
+        ),
+      ),
+    );
+  }
+}
