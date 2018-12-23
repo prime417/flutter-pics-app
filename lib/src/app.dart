702953777236b5063afc1df73361fc659c66c190
prime417 <pankajds417@gmail.com>
2018-12-23 17:20:24 +0530

    refactor to stateful widget and show a text in body

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,21 +1,28 @@
 //Import flutter helper library
 import 'package:flutter/material.dart';
 
-// Create a class that will be our custom widget
-// This class must extend the 'StatlessWidget' base class
-class App extends StatelessWidget {
-// Must define a 'build' method that returns
-// the widgets that *this* widget will show
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
+        body: Text('$counter'),
         appBar: AppBar(
           title: Text('Lets see some images!'),
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
-            print('Hi there!');
+            setState(() {
+              counter += 1;
+            });
           },
         ),
       ),
