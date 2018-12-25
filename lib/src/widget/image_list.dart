5d1fe83c1e4d1dfbf03fae6e4d961c7b2aded729
prime417 <pankajds417@gmail.com>
2018-12-25 13:13:17 +0530

    widget container created and padding, border and margin added in buildImage function

--- lib/src/widget/image_list.dart
+++ lib/src/widget/image_list.dart
@@ -9,8 +9,19 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Image.network(images[index].url);
+        return buildImage(images[index]);
       },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      margin: EdgeInsets.all(20.0),
+      child: Image.network(image.url),
     );
   }
 }
