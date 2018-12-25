409763eb46fce4d495f784cc4b1d6860ebaf29a1
prime417 <pankajds417@gmail.com>
2018-12-25 12:40:00 +0530

    added http:any dependencies and showing url to screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,9 @@
 //Import flutter helper library
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widget/image_list.dart';
 
 class App extends StatefulWidget {
   createState() {
@@ -9,21 +13,30 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
 
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text('$counter'),
+        body: ImageList(images),
         appBar: AppBar(
           title: Text('Lets see some images!'),
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          },
+          onPressed: fetchImage,
         ),
       ),
     );
